03df4154b524d8979f537c0f6077bed642d0f8b2
sraaths <sraaths@uchicago.edu>
2024-11-04 18:31:52 +0000

    cdone

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -9,5 +9,15 @@ class DirectorsController < ApplicationController
    @the_director = matching_records.at(0)
     render({ :template => "director_templates/details"})
 end
+
+
+def old
+  @eldest_director = Director.order(:dob => :desc).last
+  render({ :template => "director_templates/old"})
 end
 
+def young
+  @youngest_director = Director.order(:dob => :desc).first
+  render({ :template => "director_templates/young"})
+end
+end
